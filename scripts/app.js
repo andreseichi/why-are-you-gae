caf0dec22f39afede78053179df585e62a053d4c
André Tashiro <andre_seichi@hotmail.com>
2021-06-19 11:17:27 -0300

    toggling the divs

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,14 @@
 //   audioThank.play();
 // }, 300);
 
+const html = document.documentElement;
 const button = document.querySelector('button');
+const initialDiv = document.querySelector('.initial');
+const secondDiv = document.querySelector('.second');
 button.addEventListener('click', () => {
   const audio = new Audio('../audios/how_describe_you.mp3');
   audio.play();
+
+  initialDiv.classList.toggle('hide');
+  secondDiv.classList.toggle('hide');
 });
