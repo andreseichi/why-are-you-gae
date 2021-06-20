57752f28372798d3efc0a2503c4b9aa0550e9c52
André Tashiro <andre_seichi@hotmail.com>
2021-06-19 21:58:46 -0300

    popup functionality

--- scripts/modules/popup.js
+++ scripts/modules/popup.js
@@ -1,3 +1,15 @@
 export default function popUp() {
-  const html = document.documentElement;
+  setTimeout(() => {
+    const popup = document.querySelector('.popup-container');
+    const closeBtn = document.querySelector('.close');
+    popup.classList.add('active');
+
+    closeBtn.addEventListener('click', () => {
+      popup.remove();
+    });
+
+    setTimeout(() => {
+      popup.remove();
+    }, 4000);
+  }, 1500);
 }
