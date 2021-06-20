aaa000c066074ff688169260caadbb104b973f17
André Tashiro <andre_seichi@hotmail.com>
2021-06-19 21:58:54 -0300

    importing popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
+import popUp from './modules/popup.js';
+popUp();
+
 // setTimeout(() => {
 //   const audioThank = new Audio('../audios/thank_you.mp3');
 //   audioThank.play();
 // }, 300);
 
-const html = document.documentElement;
 const button = document.querySelector('button');
 const initialDiv = document.querySelector('.initial');
 const secondDiv = document.querySelector('.second');
